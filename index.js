dce79baa0b9041acfd70e4bd9e4c71ba68ee19f6
Vinícius Melo <98993542+Vinicius-AMelo@users.noreply.github.com>
2022-02-17 22:00:33 -0300

    alertas classList.add

--- index.js
+++ index.js
@@ -1,29 +1,32 @@
 function entrada() {
-  const form = document.querySelector('.formulario')
-  const alerta1 = document.querySelector('.alerta1')
-  const alerta2 = document.querySelector('.alerta2')
-  
 
+  // const botao = document.querySelector('.botao');
+  // const cadastro = document.querySelector('.cadastro');
+
+  const form = document.querySelector('.formulario');
+  const mensagem = document.querySelector('.mensagem')
 
   function recebeEventoForm(evento) {
     evento.preventDefault()
+
+    const alerta = document.createElement('p');
+    mensagem.innerHTML = ('');
+    mensagem.appendChild(alerta);
 
     const usuario = form.querySelector('.usuario')
     const senha = form.querySelector('.senha')
 
     if (usuario.value === '1' && senha.value === '2') {
-      alert(
-        `Login Bem Sucedido`)
-      window.open("pag002.html");
-      setTimeout(function(){
-        window.location.reload(1);
-     }, 700);
+      alerta.classList.add('login-sucess');
+      alerta.innerHTML = (`Login Bem Sucedido`)
+      form.reset();
+      usuario.focus();
+      window.open('pag002.html');
     } else {
-      alerta1.innerHTML = (`* Dados Incorretos`)
-      setTimeout(function(){
-        window.location.reload(1);
-     }, 700);
-            
+      alerta.classList.add('login-failed');
+      alerta.innerHTML = ('* Dados Incorretos')
+      form.reset();
+      usuario.focus();
     }
   }
 
